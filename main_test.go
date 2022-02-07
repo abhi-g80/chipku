12363ae9eba9d7055cfee4b3176e9f1c694e728e
Abhishek Guha <abhi.workspace@gmail.com>
2022-02-07 23:24:36 +0100

    Add test for pasteHandler

--- main_test.go
+++ main_test.go
@@ -3,8 +3,10 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -39,4 +41,32 @@ func TestRandStringBytes(t *testing.T) {
     if len(actual) != expected {
         t.Errorf("got %v bytes, expected %v,", actual, expected)
     }
+}
+
+func TestPasteHandler(t *testing.T) {
+    var str = []byte("hello")
+    req, err := http.NewRequest("POST", "/product", bytes.NewBuffer(str))
+    req.Header.Set("Content-Type", "application/json")
+
+	// req, err := http.NewRequest("POST", "", nil)
+	// In case there is an error in forming the request, we fail and stop the test
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+	hf := http.HandlerFunc(pasteHandler)
+	hf.ServeHTTP(recorder, req)
+
+	// Check the status code is what we expect.
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+    actual := strings.TrimSuffix(recorder.Body.String(), "\n")
+	if len(actual) != 6 {
+		t.Errorf("handler returned unexpected body: got %v ,%d want string of len 6", actual, len(actual))
+	}
 }
