ac04d8cd009915f967387626b428da65924e4228
Abhishek Guha <abhi.workspace@gmail.com>
2022-08-14 09:33:28 +0200

    Remove comments and make port int

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,28 +1,26 @@
 package cmd
 
 import (
-	// "fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 
 	"github.com/abhi-g80/chipku/server"
 )
 
-var port string
+var port int
 
 func init() {
 	rootCmd.AddCommand(serve)
-	serve.Flags().StringVarP(&port, "port", "p", "8080", "port to serve on")
+	serve.Flags().IntVarP(&port, "port", "p", 8080, "port to serve on")
 }
 
 var serve = &cobra.Command{
 	Use:   "serve",
 	Short: "Start server",
-	Long:  `Start the pastebin server`,
+	Long:  `Start the Chipku server`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// fmt.Printf("Chipku - a no frill pastebin v%s\n", Version)
-		server.Serve(port)
+		p := strconv.Itoa(port)
+		server.Serve(p)
 	},
 }
-
-// serve.Flags().IntVarP(&echoTimes, "times", "t", 1, "times to echo the input")
--- cmd/version.go
+++ cmd/version.go
@@ -7,17 +7,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// const Version = "0.1.1"
-
 func init() {
-	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(version)
 }
 
-var versionCmd = &cobra.Command{
+var version = &cobra.Command{
 	Use:   "version",
 	Short: "Print version",
 	Long:  `Print version number for Chipku`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Chipku - a no frill pastebin v%s\n", server.Version)
+		fmt.Printf("%s\n", server.Version)
 	},
 }
