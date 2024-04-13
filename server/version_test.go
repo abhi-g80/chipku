9d2539823437e669b640cbcbf22d9ef83ae81471
Abhishek Guha <abhi.workspace@gmail.com>
2024-04-13 13:10:18 +0200

    Make compatible with go1.22 (#13)

--- server/version_test.go
+++ server/version_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestVersion(t *testing.T) {
-	var version string = "1.1.0"
+	var version string = "1.2.0"
 
 	if version != Version {
 		t.Errorf("expected version %s, got %s", version, Version)
